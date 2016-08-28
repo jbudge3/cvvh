5ae1c56aef2d5eb090fd55129641448398ee7938
jbudge3 <jake.budge@me.com>
2016-08-27 19:28:25 -0600

    Improved hamburger menu toggle efficiency

--- code.js
+++ code.js
@@ -1,10 +1,14 @@
-function toggleBurgerMenu() {
-  if (document.getElementById("burger-menu-items").className == "toggle-burger") {
-    document.getElementById("burger-menu-items").className = " ";
-    document.getElementById("hamburger").setAttribute("fill", "#A00000");
-  } else {
-    document.getElementById("burger-menu-items").className += "toggle-burger"
-    document.getElementById("hamburger").setAttribute("fill", "green");
-  }
+// Toggle Hamburger Menu in Mobile View
 
+var burgerMenuItems = document.getElementById("burger-menu-items");
+var hamburger = document.getElementById("hamburger");
+
+function toggleBurgerMenu() {
+  if (burgerMenuItems.className == "toggle-burger") {
+    burgerMenuItems.className = " ";
+    hamburger.setAttribute("fill", "#A00000");
+  } else {
+    burgerMenuItems.className += "toggle-burger"
+    hamburger.setAttribute("fill", "green");
+  }
 }
