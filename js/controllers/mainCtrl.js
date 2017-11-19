533950249e635aabc946d7ec6a598bb78ad42166
Jake Budge <jakebudge@Jakes-iMac.local>
2017-11-19 16:12:52 -0700

    added initial markup for staff page

--- js/controllers/mainCtrl.js
+++ js/controllers/mainCtrl.js
@@ -1,3 +1,13 @@
 angular.module('cvvh').controller('mainCtrl', function($scope){
-
+    $scope.staffArray = [
+        {
+            name: "Dr. Scott Leiter, DVM",
+            imgUrl: "./img/Scott-Leiter-Circle.png",
+            pars: [
+                "Dr. Leiter graduated from Colorado State University in 1983, after completing his undergraduate studies at Brigham Young University in 1979. After nearly 2 years of working on all species of animals, he decided to focus his practice on dogs and cats. After practicing in Salt Lake City, both in day-time practices as well as evening emergency centers, he built and opened Country View Veterinary Hospital in 1996.",
+                "Dr. Leiter has served on the Ethics Committee of the utah Veterinary Community for over 10 years.",
+                "Dr. Leiter is married and is the father of two songs and a daughter, and loves spending time with his grandchildren. He loves to run and has completed over 10 full marathons."
+            ]
+        }
+    ]
 });
